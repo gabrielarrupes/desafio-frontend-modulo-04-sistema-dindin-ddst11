b38bc9ea2c47fbb8de67f14d502bc9c750457842
Gabriela Arrupes <gabrielarrupes@gmail.com>
2023-08-09 22:36:01 -0300

    fix: seta de ordenação

--- modulo-4/src/components/Table/index.jsx
+++ modulo-4/src/components/Table/index.jsx
@@ -1,18 +1,18 @@
-import "./styles.css";
 import { format } from "date-fns";
-import { useState } from "react";
 import ptBR from "date-fns/locale/pt-BR";
+import { useEffect, useState } from "react";
+import blueArrow from '../../assets/blue-arrow.svg';
 import pencil from "../../assets/pencil.svg";
-import DataController from "../DataController";
-import DeleteTransaction from "../DeleteTransaction";
 import EditModal from "../../components/EditModal/EditModal";
+import DeleteTransaction from "../DeleteTransaction";
+import "./styles.css";
 
 
 export default function Table({ transact, setTransact }) {
   const [registroId, setRegistroId] = useState("");
   const [editModal, setEditModal] = useState(false);
-
-  const arrTransact = [...transact];
+  const [sort, setSort] = useState(true);
+  const [arrTransact, setArrTransact] = useState([...transact]);
 
   const getDayOfWeek = (date) => {
     const data = new Date(date);
@@ -25,10 +25,24 @@ export default function Table({ transact, setTransact }) {
     return format(data, "dd/MM/yy", { locale: ptBR });
   };
 
+  useEffect(() => {
+    const orderedTransactions = [...transact]
+
+    if(sort) {
+      orderedTransactions.sort((a, b) => Date.parse(a.data) -  Date.parse(b.data));
+      setArrTransact([...orderedTransactions])
+      return
+      }
+
+      orderedTransactions.sort((a, b) => Date.parse(b.data) - Date.parse(a.data));
+      setArrTransact([...orderedTransactions])
+
+  }, [sort, transact]);
+
   return (
       <div className="table-container">
         <div className="table-header color000000 bg-colorFAFAFA text-align-center lato700 fsize-14">
-          <DataController transact={transact} setTransact={setTransact}  />
+        <div className='sort-date-container table-ml text-align-center table-date'>Data<img className={!sort ? "sort-date-arrow-down pointer" : "sort-date-arrow-up pointer" } src={blueArrow} alt="seta para ordenar o registro por data"  onClick={() => setSort(!sort)} /></div>
           <div className="table-day" >Dia da semana</div>
           <div className="table-description ">Descrição</div>
           <div className="table-category ">Categoria</div>
